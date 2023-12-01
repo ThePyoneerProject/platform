5237076a972d3eabe27558d3ca93d65e50969c89
chris <106525874+scriptogre@users.noreply.github.com>
2023-12-01 13:23:11 +0200

    refactor: reorganize terraform file structure
    The terraform files for the staging and production environments were reorganized for better readability, maintainability, and scalability. The resources were systematically ordered, and some repeated lines were eliminated. The changes would also make it easier for devs to pinpoint and edit configurations quickly.

--- terraform/production.tf
+++ terraform/production.tf
@@ -5,12 +5,56 @@ resource "heroku_app" "production" {
   stack  = "container"
 }
 
-# Associate a custom domain
+# Set the build to the production app
+resource "heroku_build" "production" {
+  app_id = heroku_app.production.id
+  source {
+    path = "../app"
+  }
+}
+
+# Create a heroku formation for the production app
+resource "heroku_formation" "production" {
+  app_id = heroku_app.production.id
+  type   = "web"
+  quantity = 1
+  size   = "Basic"
+
+  depends_on = [
+    heroku_build.production,
+    heroku_addon.production_postgres,
+    heroku_addon.production_redis
+  ]
+}
+
+# Set the config vars for the production app
+resource "heroku_app_config_association" "production" {
+  app_id         = heroku_app.production.id
+  vars           = merge(local.common_vars, { ENVIRONMENT_TYPE = "production" })
+  sensitive_vars = local.sensitive_vars
+}
+
+# Create a Postgres addon for the production app
+resource "heroku_addon" "production_postgres" {
+  app_id = heroku_app.production.id
+  plan   = "heroku-postgresql:mini"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Create a Redis addon for the production app
+resource "heroku_addon" "production_redis" {
+  app_id = heroku_app.production.id
+  plan   = "heroku-redis:mini"
+}
+
+# Create custom domains in Heroku (non-www and www)
 resource "heroku_domain" "production_non_www" {
   app_id   = heroku_app.production.id
   hostname = "pyoneers.dev"
 }
-
 resource "heroku_domain" "production_www" {
   app_id   = heroku_app.production.id
   hostname = "www.pyoneers.dev"
@@ -50,54 +94,9 @@ resource "herokux_app_github_integration" "production" {
   depends_on = [herokux_pipeline_github_integration.default]
 }
 
-# Create a heroku formation for the production app
-resource "heroku_formation" "production" {
-  app_id = heroku_app.production.id
-  type   = "web"
-  quantity = 1
-  size   = "Basic"
-
-  depends_on = [
-    heroku_build.production,
-    heroku_addon.production_postgres,
-    heroku_addon.production_redis
-  ]
-}
-
-# Set the build to the production app
-resource "heroku_build" "production" {
-  app_id = heroku_app.production.id
-  source {
-    path = "../app"
-  }
-}
-
-# Create a Postgres addon for the production app
-resource "heroku_addon" "production_postgres" {
-  app_id = heroku_app.production.id
-  plan   = "heroku-postgresql:mini"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-# Create a Redis addon for the production app
-resource "heroku_addon" "production_redis" {
-  app_id = heroku_app.production.id
-  plan   = "heroku-redis:mini"
-}
-
 # Add the production app to the pipeline
 resource "heroku_pipeline_coupling" "production" {
   app_id    = heroku_app.production.id
   pipeline  = heroku_pipeline.default.id
   stage     = "production"
-}
-
-# Set the config vars for the production app
-resource "heroku_app_config_association" "production" {
-  app_id         = heroku_app.production.id
-  vars           = merge(local.common_vars, { ENVIRONMENT_TYPE = "production" })
-  sensitive_vars = local.sensitive_vars
 }
