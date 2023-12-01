ddb1f1f30ee1dbc689d7cb8b47be95dc9e048872
chris <106525874+scriptogre@users.noreply.github.com>
2023-12-01 13:23:11 +0200

    refactor: reorganize terraform file structure
    The terraform files for the staging and production environments were reorganized for better readability, maintainability, and scalability. The resources were systematically ordered, and some repeated lines were eliminated. The changes would also make it easier for devs to pinpoint and edit configurations quickly.

--- terraform/staging.tf
+++ terraform/staging.tf
@@ -1,44 +1,19 @@
-# Create a staging app
+# Create the Heroku app
 resource "heroku_app" "staging" {
   name   = "django-staging"
   region = "eu"
   stack  = "container"
 }
 
-# Associate a custom domain
-resource "heroku_domain" "staging" {
-  app_id   = heroku_app.staging.id
-  hostname = "staging.pyoneers.dev"
-}
-
-# Configure Namecheap DNS records to point to the Heroku DNS target
-resource "namecheap_domain_records" "staging" {
-    domain = "pyoneers.dev"
-    mode = "MERGE"
-
-    record {
-      hostname = "staging"
-      type = "CNAME"
-      address = "ssl.autoidleapp.com"
-      ttl = 60
-    }
-
-  # Ensure DNS records are set after the Heroku domain is created
-  depends_on = [heroku_domain.staging]
-}
-
-# Add Heroku app GitHub integration.
-resource "herokux_app_github_integration" "staging" {
+# Set the Heroku build
+resource "heroku_build" "staging" {
   app_id = heroku_app.staging.id
-  branch = "staging"
-  auto_deploy = true
-
-  # Tells Terraform that this resource must be created/updated
-  # only after the `herokux_pipeline_github_integration` has been successfully applied.
-  depends_on = [herokux_pipeline_github_integration.default]
+  source {
+    path = "../app"
+  }
 }
 
-# Create a heroku formation for the staging app
+# Set the Heroku formation (dynos)
 resource "heroku_formation" "staging" {
   app_id = heroku_app.staging.id
   type   = "web"
@@ -52,30 +27,59 @@ resource "heroku_formation" "staging" {
   ]
 }
 
-# Set the build to the staging app
-resource "heroku_build" "staging" {
-  app_id = heroku_app.staging.id
-  source {
-    path = "../app"
-  }
+# Set the config vars
+resource "heroku_app_config_association" "staging" {
+  app_id         = heroku_app.staging.id
+  vars           = merge(local.common_vars, { ENVIRONMENT_TYPE = "staging" })
+  sensitive_vars = local.sensitive_vars
 }
 
-# Create an AutoIdle addon for the staging app
+# Create an AutoIdle addon
 resource "heroku_addon" "auto_idle" {
   app_id = heroku_app.staging.id
   plan   = "autoidle:hobby"
 }
 
-# Create a Postgres addon for the staging app
+# Create a Postgres addon
 resource "heroku_addon" "staging_postgres" {
   app_id = heroku_app.staging.id
   plan   = "heroku-postgresql:mini"
 }
 
-# Create a Redis addon for the staging app
+# Create a Redis addon
 resource "heroku_addon" "staging_redis" {
   app_id = heroku_app.staging.id
   plan   = "heroku-redis:mini"
+}
+
+# Create a custom domain in Heroku
+resource "heroku_domain" "staging" {
+  app_id   = heroku_app.staging.id
+  hostname = "staging.pyoneers.dev"
+}
+
+# Configure Namecheap DNS records to point to the Heroku DNS target
+resource "namecheap_domain_records" "staging" {
+    domain = "pyoneers.dev"
+    mode = "MERGE"
+
+    record {
+      hostname = "staging"
+      type = "CNAME"
+      address = "ssl.autoidleapp.com"  # This avoids Heroku from throwing errors when the app is idle
+      ttl = 60
+    }
+
+  depends_on = [heroku_domain.staging]
+}
+
+# Add GitHub integration for deployment upon pushing to the `staging` branch
+resource "herokux_app_github_integration" "staging" {
+  app_id = heroku_app.staging.id
+  branch = "staging"
+  auto_deploy = true
+
+  depends_on = [herokux_pipeline_github_integration.default]
 }
 
 # Add the staging app to the pipeline
@@ -83,11 +87,4 @@ resource "heroku_pipeline_coupling" "staging" {
   app_id    = heroku_app.staging.id
   pipeline  = heroku_pipeline.default.id
   stage     = "staging"
-}
-
-# Set the config vars for the staging app
-resource "heroku_app_config_association" "staging" {
-  app_id         = heroku_app.staging.id
-  vars           = merge(local.common_vars, { ENVIRONMENT_TYPE = "staging" })
-  sensitive_vars = local.sensitive_vars
 }
